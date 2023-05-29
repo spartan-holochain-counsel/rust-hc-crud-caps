10d042c36024e2d839008bdb621595a8c09f0b74
Matthew Brisebois <matthew@webheroes.ca>
2023-05-29 13:11:24 -0600

    Support for Holochain v0.1.5-beta-rc.1

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Holochain Development Env";
+
+  inputs = {
+    nixpkgs.follows = "holochain-flake/nixpkgs";
+    flake-parts.follows = "holochain-flake/flake-parts";
+    holochain-nix-versions.url  = "github:holochain/holochain/?dir=versions/0_2";
+
+    holochain-flake = {
+      url = "github:holochain/holochain";
+      inputs.holochain.url = "github:holochain/holochain/holochain-0.1.5-beta-rc.1";
+      inputs.lair.url = "github:holochain/lair/lair_keystore-v0.2.3";
+    };
+  };
+
+  outputs = inputs @ { ... }:
+    inputs.holochain-flake.inputs.flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      {
+        systems = builtins.attrNames inputs.holochain-flake.devShells;
+        perSystem =
+          { config
+          , pkgs
+          , system
+          , ...
+          }: {
+            devShells.default = pkgs.mkShell {
+              inputsFrom = [ inputs.holochain-flake.devShells.${system}.holonix ];
+              packages = with pkgs; [
+              ];
+            };
+          };
+      };
+}
